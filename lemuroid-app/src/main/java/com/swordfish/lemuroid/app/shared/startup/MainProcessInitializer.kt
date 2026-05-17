af7328803bf46581de6ffb0b1d6720fd09261f05
eliott <eliott.vannuffel@proton.me>
2026-05-17 09:18:39 +0200

    feat(cheats): download cheat database on app startup

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/startup/MainProcessInitializer.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/startup/MainProcessInitializer.kt
@@ -5,13 +5,22 @@ import androidx.startup.Initializer
 import androidx.work.WorkManagerInitializer
 import com.swordfish.lemuroid.app.shared.library.LibraryIndexScheduler
 import com.swordfish.lemuroid.app.shared.savesync.SaveSyncWork
+import com.swordfish.lemuroid.lib.cheats.CheatDatabaseDownloader
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
-class MainProcessInitializer : Initializer<Unit> {
+class MainProcessInitializer(
+    private val cheatDatabaseDownloader: CheatDatabaseDownloader,
+) : Initializer<Unit> {
     override fun create(context: Context) {
         Timber.i("Requested initialization of main process tasks")
         SaveSyncWork.enqueueAutoWork(context, 0)
         LibraryIndexScheduler.scheduleCoreUpdate(context)
+        CoroutineScope(Dispatchers.IO).launch {
+            cheatDatabaseDownloader.ensureCheatDatabaseDownloaded()
+        }
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
